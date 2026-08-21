16819b29dab2485b313e7f4fbc67695d5443e42b
SRougee <ronaldjennings84@gmail.com>
2026-08-21 13:22:58 +0200

    Add MSTest, NUnit, xUnit test projects to solution
    Added BankCore.Tests.MSTest, BankCore.Tests.NUnit, and BankCore.Tests.xUnit projects targeting .NET 8.0, referencing core/data libraries, and configured with FluentAssertions, Moq, coverlet.collector, and respective test frameworks. Added initial test classes with basic tests. Updated solution file to include new projects and build configs. Enabled parallel test execution for MSTest via MSTestSettings.cs.

---
+++ BankCore.Tests.MSTest/Test1.cs
@@ -0,0 +1,10 @@
+﻿namespace BankCore.Tests.MSTest;
+
+[TestClass]
+public sealed class Test1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
---
+++ BankCore.Tests.NUnit/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace BankCore.Tests.NUnit;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
